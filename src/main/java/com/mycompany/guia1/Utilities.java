3b3f90d1b26df764795831093fee7a3d07c0df6e
usuario <usuario@com2pc15.uesocc.edu.sv>
2017-08-17 06:49:10 -0600

    Hice el primer metodo

---
+++ src/main/java/com/mycompany/guia1/Utilities.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.guia1;
+
+/**
+ *
+ * @author usuario
+ */
+public class Utilities {
+    
+    public String getResume(String texto){
+        String Resu="";
+        for(int principal=0;principal<30;principal ++){
+            Resu= Resu+texto.charAt(principal);
+        }
+        return Resu;
+    }
+    public String capitalizar(String texto){
+       
+        
+        
+        
+        return null;
+        
+    }
+ 
+    
+    
+    
+}
